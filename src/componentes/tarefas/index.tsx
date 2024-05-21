49ac7e17d7667c6903e1ddf249ca592f88da846b
João Colares <joaocolaresm@gmail.com>
2024-05-21 17:09:40 -0300

    projeto_05_pt02_aula09

--- src/componentes/tarefas/index.tsx
+++ src/componentes/tarefas/index.tsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from 'react'
 import { useDispatch } from 'react-redux'
 
 import * as S from './styles'
 
-import {remover, editar} from '../../store/reducers/tarefas'
+import { remover, editar, alteraStatus } from '../../store/reducers/tarefas'
 import TarefaClass from '../../models/Tarefas'
-import { BotaoSalvar } from '../../styles'
+import { Botao, BotaoSalvar } from '../../styles'
+
+import * as enums from '../../utils/enums/Tarefa'
 
 type Props = TarefaClass
 
@@ -15,7 +17,7 @@ const Tarefa = ({
   status,
   titulo,
   id
-} : Props) => {
+}: Props) => {
   const dispatch = useDispatch()
   const [estaEditando, setEstaEditando] = useState(false)
   const [descricao, setDescricao] = useState('')
@@ -31,24 +33,44 @@ const Tarefa = ({
     setDescricao(descricaoOriginal)
   }
 
+  function alteraStatusTarefa(evento: ChangeEvent<HTMLInputElement>) {
+    dispatch(
+      alteraStatus({
+        id,
+        finalizado: evento.target.checked,
+      })
+    )
+  }
+
   return (
     <S.Card>
-      <S.Titulo>{titulo}</S.Titulo>
-      <S.Tag parametro='prioridade' prioridade = {prioridade}>
+      <label htmlFor={titulo}>
+        <input
+        type="checkbox"
+        id={titulo}
+        checked={status === enums.Status.CONCLUIDA}
+        onChange={alteraStatusTarefa}
+        />
+        <S.Titulo>
+          {estaEditando && <em>Editando: </em>}
+          {titulo}
+        </S.Titulo>
+      </label>
+      <S.Tag parametro='prioridade' prioridade={prioridade}>
         {prioridade}
       </S.Tag>
-      <S.Tag parametro='status' status = {status}>
+      <S.Tag parametro='status' status={status}>
         {status}
       </S.Tag>
       <S.Descricao
-      disabled={!estaEditando}
-      value={descricao}
-      onChange={(evento) => setDescricao(evento.target.value)}
+        disabled={!estaEditando}
+        value={descricao}
+        onChange={(evento) => setDescricao(evento.target.value)}
       />
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-              <BotaoSalvar
+            <BotaoSalvar
               onClick={() => {
                 dispatch(
                   editar({
@@ -61,19 +83,19 @@ const Tarefa = ({
                 )
                 setEstaEditando(false)
               }}>Salvar</BotaoSalvar>
-              <S.BotaoCancelarRemover onClick={cancelarEdicao}>
-                Cancelar
-              </S.BotaoCancelarRemover>
+            <S.BotaoCancelarRemover onClick={cancelarEdicao}>
+              Cancelar
+            </S.BotaoCancelarRemover>
           </>
         ) : (
           <>
-          <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
-          <S.BotaoCancelarRemover onClick={() => dispatch(remover(id))}>Remover</S.BotaoCancelarRemover>
+            <Botao onClick={() => setEstaEditando(true)}>Editar</Botao>
+            <S.BotaoCancelarRemover onClick={() => dispatch(remover(id))}>Remover</S.BotaoCancelarRemover>
           </>
         )}
       </S.BarraAcoes>
     </S.Card>
-)
+  )
 }
 
 export default Tarefa
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,19 +1,28 @@
 import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
 import FiltroCard from "../../componentes/FiltroCard"
 import { RootReducer } from "../../store"
 import { alterarTermo } from "../../store/reducers/filtro"
 
 import * as S from './styles'
 import * as enums from '../../utils/enums/Tarefa'
-import { Campo } from "../../styles"
+import { Botao, Campo } from "../../styles"
 
-const BarraLateral = () => {
+type Props = {
+  mostrarFiltros: boolean
+}
+
+const BarraLateral = ({mostrarFiltros}:Props) => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const { termo } = useSelector((state: RootReducer) => state.filtro)
 
   return (
     <S.Aside>
       <div>
+        {mostrarFiltros ? (
+        <>
         <Campo
           type="text"
           placeholder="Buscar"
@@ -48,6 +57,10 @@ const BarraLateral = () => {
           />
           <FiltroCard criterio='todas' legenda="todas" />
         </S.Filtros>
+        </>
+        ) : (
+          <Botao onClick={() => navigate('/')}>Voltar a lista de tarefas</Botao>
+        )}
       </div>
     </S.Aside>
   )
